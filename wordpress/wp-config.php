64569133bcce2c4318889bdf2f32417ebbf888f1
scsivaprakasam <sc.sivaprakasam@gmail.com>
2023-04-19 12:48:55 -0500

    Parameteriezed

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -23,17 +23,21 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+/* define( 'DB_NAME', 'wordpress' ); */
+define( 'DB_NAME', '_DB_NAME_' );
 
 /** Database username */
-define( 'DB_USER', 'wpuser' );
+/* define( 'DB_USER', 'wpuser' ); */
+define( 'DB_USER', '_DB_USER_' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Wp@dmin123' );
+/* define( 'DB_PASSWORD', 'Wp@dmin123' ); */
+define( 'DB_PASSWORD', '_DB_PASSWORD_' );
 
 /** Database hostname */
 /** define( 'DB_HOST', 'mydemowp.mysql.database.azure.com' );*/
-define( 'DB_HOST', 'mydemowppvt.mysql.database.azure.com' ); 
+/* define( 'DB_HOST', 'mydemowppvt.mysql.database.azure.com' ); */
+define( 'DB_HOST', '_DB_HOST_' ); 
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
